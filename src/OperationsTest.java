6b9a6376b847d8f154350b5f48e8e17b582c9e96
Greg McLauchlan <42975536+gmclauchlan19@users.noreply.github.com>
2018-09-28 12:27:34 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,22 @@
+
+public class OperationsTest
+{
+
+	public static void main(String[] args)
+	{
+		int number; //Declaration statement
+		number = 20;  //Assignment statement -> initialization
+		number = number + 5;  // number+= 5
+		System.out.println(number);
+		number/=2;
+		System.out.println(number);
+		number-=6;
+		System.out.println(number);
+		number*=4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+
+	}
+
+}
